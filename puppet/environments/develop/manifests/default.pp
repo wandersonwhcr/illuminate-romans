4b376026ae8bd3fc480c30c46bf9074e7e3fcadc
Wanderson <wandersonwhcr@gmail.com>
2017-09-26 22:32:30 -0300

    Install HTTPS packages for apt-get

---
+++ puppet/environments/develop/manifests/default.pp
@@ -0,0 +1,26 @@
+Exec {
+    path    => ["/usr/bin", "/bin", "/usr/sbin", "/sbin"],
+    timeout => 0,
+}
+
+Package {
+    require => Exec["apt-get : update"],
+    before  => Exec["apt-get : autoremove"],
+}
+
+exec { "apt-get : update":
+    command => "apt-get update",
+}
+
+exec { "apt-get : autoremove":
+    command => "apt-get autoremove -q -y",
+}
+
+exec { "apt-get : https-update":
+    command => "apt-get update",
+}
+
+package { "apt-get : https":
+    name    => "apt-transport-https",
+    require => Exec["apt-get : https-update"],
+}
